683cf3e75ba0af8a1d5749d67151866636078916
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-10-05 20:37:40 -0300

    Add tests to IngresoNombresTest

--- src/test/java/org/example/IngresoNombresTest.java
+++ src/test/java/org/example/IngresoNombresTest.java
@@ -6,20 +6,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class IngresoNombresTest {
+    private IngresoNombres instanciaNueva;
 
     @BeforeEach
     void setUp() {
+        // Creo esa instancia para acceder a los metodos de Ingreso Nombres
+        // en cada test que hago
+        instanciaNueva = new IngresoNombres();
     }
 
     @Test
     void getNombreCompleto() {
+        // Le seteo el nombre y despues el apellido y despues compruebo si coincide
+        instanciaNueva.setPrimerNombre("Bart");
+        instanciaNueva.setPrimerApellido("Simpson");
+        assertEquals("Bart Simpson", instanciaNueva.getNombreCompleto());
     }
 
     @Test
     void getPrimerNombre() {
+        instanciaNueva.setPrimerNombre("bart");
+        assertEquals("bart", instanciaNueva.getPrimerNombre());
     }
 
     @Test
     void getPrimerApellido() {
+        instanciaNueva.setPrimerApellido("simpson");
+        assertEquals("simpson", instanciaNueva.getPrimerApellido());
+
     }
 }
